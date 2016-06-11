899b47f4a65e3e6d187fe84e7324398795767dbc
Ambar Sethi <ambarrrsethi@gmail.com>
2016-06-11 10:17:39 +0100

    color pallete

---
+++ main.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  console.log("%cMWH Gardening Services" + "  " + "%cmwh.gardening@gmail.com", "color:green; background:orange; font-size: 16pt", "color:red; background:green; font-size: 16pt");
+
+});
